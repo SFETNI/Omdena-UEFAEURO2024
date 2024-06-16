b3bbd0729063b87d546566816019489249a92efe
Zoran Radovanovic <zoranrdc@yahoo.com>
2024-06-16 23:33:25 +0200

    Improved performance of the create_actions_grid function. Increased the number of tiles for the football pitch to get more precise results.

--- src/calculate_actions_grid.py
+++ src/calculate_actions_grid.py
@@ -15,7 +15,7 @@ pd.options.mode.copy_on_write = True
 logger = logging.getLogger(__name__)
 STATSBOMB_OPEN_DATA_LOCAL_PATH = "C:\projects\statsbomb_open_data"
 FOOTBALL_PITCH_SIZE = (120, 80)
-HEATMAP_TILE_SIZE = (10, 10)
+HEATMAP_TILE_SIZE = (2, 2)
 
 
 def fill_actions_grid(row: object, actions_grid: pd.DataFrame, tile_width: int, tile_height: int, max_horizontal_tiles: int):
@@ -26,9 +26,6 @@ def fill_actions_grid(row: object, actions_grid: pd.DataFrame, tile_width: int, 
     horizontal_tile_index = int(x_loc/tile_width)
     vertical_tile_index = int(y_loc/tile_height)
     grid_index = vertical_tile_index * max_horizontal_tiles + horizontal_tile_index
-    if grid_index > 95:
-        logger.debug("grid_index > 95")
-        logger.debug(row)
     action_grid_filter = (actions_grid["player_id"] == row["player_id"]) & (actions_grid["grid_index"] == grid_index)
     if len(actions_grid.loc[action_grid_filter].index) == 0:
         print(f"row: {row}")
@@ -131,19 +128,25 @@ def get_actions(df_events: pd.DataFrame):
     action_types_filter = ((df_events.type == "Dribble") | (df_events.type == "Shot") |
                            ((df_events.type == "Pass") & df_events.pass_cross))
     df_actions = df_events.loc[action_types_filter].reset_index()
+    df_actions["grid_index"] = -1
+    df_actions[np.array(df_actions.type.unique())] = 0
+    df_actions[["loc_x", "loc_y"]] = df_actions["location"].tolist()
+    df_actions.loc[df_actions["loc_x"] > 119, "loc_x"] = 119
+    df_actions.loc[df_actions["loc_y"] > 79, "loc_x"] = 79
 
     return df_actions
 
 
 def create_actions_grid(df_actions: pd.DataFrame, football_pitch_size, heatmap_tile_size):
-    actions_grid_width = int(football_pitch_size[0] / heatmap_tile_size[0])
-    actions_grid_height = int(football_pitch_size[1] / heatmap_tile_size[1])
-    df_actions_grid_positions = pd.DataFrame(np.array(range(actions_grid_width * actions_grid_height)), columns=["grid_index"])
-    df_actions_grid = df_actions[["player_id"]].drop_duplicates()
-    df_actions_grid[np.array(df_actions.type.unique())] = 0
-    df_actions_grid = df_actions_grid.join(df_actions_grid_positions, how="cross")
-    df_actions.apply(fill_actions_grid, axis=1, args=(df_actions_grid, heatmap_tile_size[0], heatmap_tile_size[1],
-                                                      int(football_pitch_size[0] / heatmap_tile_size[0])))
+    max_horizontal_tiles = int(football_pitch_size[0] / heatmap_tile_size[0])
+    df_actions["grid_index"] = ((df_actions["loc_y"] / heatmap_tile_size[1]).astype(int) * max_horizontal_tiles +
+                                (df_actions["loc_x"] / heatmap_tile_size[0]).astype(int))
+    df_actions.loc[df_actions["type"] == "Shot", "Shot"] = 1
+    df_actions.loc[df_actions["type"] == "Pass", "Pass"] = 1
+    df_actions.loc[df_actions["type"] == "Dribble", "Dribble"] = 1
+
+    df_actions_grid = df_actions[["player_id", "Shot", "Pass", "Dribble", "grid_index"]]
+    df_actions_grid = df_actions_grid.groupby(["player_id", "grid_index"], as_index=False).sum()
 
     return df_actions_grid
 
@@ -152,11 +155,10 @@ if __name__ == "__main__":
     logging.basicConfig(filename=r"..\output\app.log", level=logging.DEBUG)
     matches_ids = get_matches_ids(os.path.join(STATSBOMB_OPEN_DATA_LOCAL_PATH, "data", "events"))
     start_time = time.time()
-    df_events = load_events(matches_ids, parallel_processes_count=4)
+    df_events = load_events(matches_ids, parallel_processes_count=8)
+    #df_events = load_events_single_proc(matches_ids)
     end_time = time.time()
     print(f"Load events elapsed time: {end_time-start_time}s")
     df_actions = get_actions(df_events)
     df_actions_grid = create_actions_grid(df_actions, FOOTBALL_PITCH_SIZE, HEATMAP_TILE_SIZE)
     df_actions_grid.to_csv(r"..\output\actions_grid.csv", index=False)
-
-    print(df_actions_grid)
