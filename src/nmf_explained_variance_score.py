60075cc59c76790b205b3e6be31a8f43eeb5d80b
Zoran Radovanovic <zoranrdc@yahoo.com>
2024-06-22 12:10:53 +0200

    The NMF explained variance score is now calculated in a single run.

--- src/nmf_explained_variance_score.py
+++ src/nmf_explained_variance_score.py
@@ -5,26 +5,16 @@ import pandas as pd
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import explained_variance_score
 
-# Thomas Müller
-PLAYER_ID = 5562
-# Raquel Rodriguez
-#PLAYER_ID = 4967
-# Paulo Dybala
-#PLAYER_ID = 5743
-# Messi
-#PLAYER_ID = 5503
-ACTION_TYPE = "Dribble"
-action_heatmap = np.load(os.path.join(consts.OUTPUT_DIR_PATH, f"actions_heatmap_{ACTION_TYPE}.npy"))
-action_heatmap_H = np.load(os.path.join(consts.OUTPUT_DIR_PATH, f"compressed_heatmap_{ACTION_TYPE}_H.npy"))
-action_heatmap_W = np.load(os.path.join(consts.OUTPUT_DIR_PATH, f"compressed_heatmap_{ACTION_TYPE}_W.npy"))
-reconstructed_action_heatmap = np.matmul(action_heatmap_W, action_heatmap_H)
-actions_grid_smoothed = pd.read_csv(os.path.join(consts.OUTPUT_DIR_PATH, "actions_grid_smoothed.csv"))
-row_index = np.where(actions_grid_smoothed["player_id"].unique() == PLAYER_ID)[0]
-#score = explained_variance_score(action_heatmap[row_index], reconstructed_action_heatmap[row_index])
-#mse = mean_squared_error(action_heatmap[row_index], reconstructed_action_heatmap[row_index])
-score = explained_variance_score(action_heatmap, reconstructed_action_heatmap)
-mse = mean_squared_error(action_heatmap, reconstructed_action_heatmap)
 
-print(f"Explained variance score for action type '{ACTION_TYPE} is {score}.")
-print(f"MSE for action type '{ACTION_TYPE} is {mse}.")
+for action_type in ["Pass", "Dribble", "Shot", "Cross"]:
+    action_heatmap = np.load(os.path.join(consts.OUTPUT_DIR_PATH, f"actions_heatmap_{action_type}.npy"))
+    action_heatmap_H = np.load(os.path.join(consts.OUTPUT_DIR_PATH, f"compressed_heatmap_{action_type}_H.npy"))
+    action_heatmap_W = np.load(os.path.join(consts.OUTPUT_DIR_PATH, f"compressed_heatmap_{action_type}_W.npy"))
+    reconstructed_action_heatmap = np.matmul(action_heatmap_W, action_heatmap_H)
+    actions_grid_smoothed = pd.read_csv(os.path.join(consts.OUTPUT_DIR_PATH, "start_actions_grid_smoothed.csv"))
+    score = explained_variance_score(action_heatmap, reconstructed_action_heatmap)
+    mse = mean_squared_error(action_heatmap, reconstructed_action_heatmap)
+
+    print(f"Explained variance score for action type '{action_type} is {score}.")
+    print(f"MSE for action type '{action_type} is {mse}.")
 
