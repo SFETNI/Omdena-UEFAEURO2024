e86907c9946e1d76907848c3f611a80cd4bdf8e8
Zoran Radovanovic <zoranrdc@yahoo.com>
2024-06-22 12:07:05 +0200

    Added constant for the file path where the values for the ending location of an action are stored.

--- src/consts.py
+++ src/consts.py
@@ -10,12 +10,13 @@ ACTIONS_GRID_FILE_PATH = os.path.join(OUTPUT_DIR_PATH, "actions_grid.csv")
 PLAYERS_PLAYED_TIME_FILE_PATH = os.path.join(OUTPUT_DIR_PATH, "players_played_time.csv")
 PLAYERS_TOTAL_PLAYED_TIME_FILE_PATH = os.path.join(OUTPUT_DIR_PATH, "players_total_played_time.csv")
 ACTIONS_GRID_NORMALIZED_FILE_PATH = os.path.join(OUTPUT_DIR_PATH, "actions_grid_normalized.csv")
-ACTIONS_GRID_SMOOTHED_FILE_PATH = os.path.join(OUTPUT_DIR_PATH, "actions_grid_smoothed.csv")
+START_ACTIONS_GRID_SMOOTHED_FILE_PATH = os.path.join(OUTPUT_DIR_PATH, "start_actions_grid_smoothed.csv")
+END_ACTIONS_GRID_SMOOTHED_FILE_PATH = os.path.join(OUTPUT_DIR_PATH, "end_actions_grid_smoothed.csv")
 STATSBOMB_OPEN_DATA_LOCAL_PATH = os.environ["OPEN_DATA_REPO_PATH"]
 FOOTBALL_PITCH_SIZE = (120, 80)
 HEATMAP_TILE_SIZE = (10, 10)
 ACTIONS_GRID_SHAPE = (int(FOOTBALL_PITCH_SIZE[0]/HEATMAP_TILE_SIZE[0]), int(FOOTBALL_PITCH_SIZE[1]/HEATMAP_TILE_SIZE[1]))
-COMPONENTS_PER_ACTION = { "Shot": 6, "Pass": 6, "Dribble": 40 }
+COMPONENTS_PER_ACTION = { "Shot": 6, "Pass": 10, "Cross": 6, "Dribble": 10 }
 
 
 Path(OUTPUT_DIR_PATH).mkdir(parents=True, exist_ok=True)
