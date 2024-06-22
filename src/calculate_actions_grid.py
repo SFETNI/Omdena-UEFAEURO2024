5100721e852492fe7a7e9d1b07222fc45456f19c
Zoran Radovanovic <zoranrdc@yahoo.com>
2024-06-22 12:05:48 +0200

    The action's ending location is also add to the action's grid data. Fixed error with replacement of NaN values in the pass_cross column.

--- src/calculate_actions_grid.py
+++ src/calculate_actions_grid.py
@@ -31,8 +31,9 @@ def fill_actions_grid(row: object, actions_grid: pd.DataFrame, tile_width: int, 
 
 def load_events_worker(matches_queue: queue.Queue, events_queue: queue.Queue):
     df_all_events = pd.DataFrame(np.array([], dtype=[("player_id", np.int32),
-                                                     ("type", str), ("pass_cross", bool), ("location", object)]),
-                                 columns=["player_id", "type", "pass_cross", "location"])
+                                                     ("type", str), ("pass_cross", bool),
+                                                     ("location", object), ("pass_end_location", object)]),
+                                 columns=["player_id", "type", "pass_cross", "location", "pass_end_location"])
     matches_ids = matches_queue.get()
 
     for match_id in matches_ids:
@@ -41,19 +42,21 @@ def load_events_worker(matches_queue: queue.Queue, events_queue: queue.Queue):
         # Filter out starting events such as kick off event.
         df_events = df_events.loc[df_events["player_id"].notna()]
         df_events["player_id"] = df_events["player_id"].astype(int)
+        df_events["pass_cross"] = df_events["pass_cross"].fillna(False)
         if df_all_events is None:
-            df_all_events = df_events[["player_id", "type", "pass_cross", "location"]]
+            df_all_events = df_events[["player_id", "type", "pass_cross", "location", "pass_end_location"]]
         else:
-            df_all_events = pd.concat([df_all_events, df_events[["player_id", "type", "pass_cross", "location"]]])
+            df_all_events = pd.concat([df_all_events, df_events[["player_id", "type", "pass_cross", "location", "pass_end_location"]]])
 
     events_queue.put(df_all_events)
     matches_queue.task_done()
 
 
 def load_events_worker_single_proc(matches_ids):
-    df_all_events = pd.DataFrame(np.array([], dtype=[("player_id", np.int32),
-                                                     ("type", str), ("pass_cross", bool), ("location", object)]),
-                                 columns=["player_id", "type", "pass_cross", "location"])
+    df_all_events = pd.DataFrame(np.array([], dtype=[("player_id", np.int32), ("type", str),
+                                                     ("pass_cross", bool), ("location", object),
+                                                     ("pass_end_location", object)]),
+                                 columns=["player_id", "type", "pass_cross", "location", "pass_end_location"])
 
     for match_id in matches_ids:
         print(f"Loading events data for match with id: {match_id}")
@@ -61,10 +64,11 @@ def load_events_worker_single_proc(matches_ids):
         # Filter out starting events such as kick off event.
         df_events = df_events.loc[df_events["player_id"].notna()]
         df_events["player_id"] = df_events["player_id"].astype(int)
+        df_events["pass_cross"] = df_events["pass_cross"].astype(bool)
         if df_all_events is None:
-            df_all_events = df_events[["player_id", "type", "pass_cross", "location"]]
+            df_all_events = df_events[["player_id", "type", "pass_cross", "location", "pass_end_location"]]
         else:
-            df_all_events = pd.concat([df_all_events, df_events[["player_id", "type", "pass_cross", "location"]]])
+            df_all_events = pd.concat([df_all_events, df_events[["player_id", "type", "pass_cross", "location", "pass_end_location"]]])
 
     return df_all_events
 
@@ -122,13 +126,22 @@ def get_matches_ids(data_path: str, max_events=-1):
 
 def get_actions(df_events: pd.DataFrame):
     action_types_filter = ((df_events.type == "Dribble") | (df_events.type == "Shot") |
-                           ((df_events.type == "Pass") & df_events.pass_cross))
+                           (df_events.type == "Pass"))
     df_actions = df_events.loc[action_types_filter].reset_index()
     df_actions["grid_index"] = -1
+    df_actions["end_grid_index"] = -1
     df_actions[np.array(df_actions.type.unique())] = 0
+    df_actions.loc[df_actions.pass_end_location.isnull(), 'pass_end_location'] = df_actions.loc[
+        df_actions.pass_end_location.isnull(), 'pass_end_location'].apply(lambda x: [-1, -1])
     df_actions[["loc_x", "loc_y"]] = df_actions["location"].tolist()
+    df_actions[["end_loc_x", "end_loc_y"]] = df_actions["pass_end_location"].tolist()
     df_actions.loc[df_actions["loc_x"] > 119, "loc_x"] = 119
-    df_actions.loc[df_actions["loc_y"] > 79, "loc_x"] = 79
+    df_actions.loc[df_actions["loc_y"] > 79, "loc_y"] = 79
+    df_actions.loc[df_actions["end_loc_x"] > 119, "end_loc_x"] = 119
+    df_actions.loc[df_actions["end_loc_y"] > 79, "end_loc_y"] = 79
+    df_actions["pass_cross"] = df_actions["pass_cross"].fillna(False)
+    # Add additional column for cross passes.
+    df_actions["Cross"] = 0
 
     return df_actions
 
@@ -137,12 +150,17 @@ def create_actions_grid(df_actions: pd.DataFrame, football_pitch_size, heatmap_t
     max_horizontal_tiles = int(football_pitch_size[0] / heatmap_tile_size[0])
     df_actions["grid_index"] = ((df_actions["loc_y"] / heatmap_tile_size[1]).astype(int) * max_horizontal_tiles +
                                 (df_actions["loc_x"] / heatmap_tile_size[0]).astype(int))
+    df_actions.loc[df_actions.end_loc_x > -1, "end_grid_index"] = ((df_actions["end_loc_y"] / heatmap_tile_size[1]).astype(int) * max_horizontal_tiles +
+                                (df_actions["end_loc_x"] / heatmap_tile_size[0]).astype(int))
     df_actions.loc[df_actions["type"] == "Shot", "Shot"] = 1
     df_actions.loc[df_actions["type"] == "Pass", "Pass"] = 1
+    # We need to check if the type of action is "Pass" because "pass_cross" is set to true even on the rows
+    # containing other action types such as "Dribble".
+    df_actions.loc[(df_actions["type"] == "Pass") & df_actions["pass_cross"], "Cross"] = 1
     df_actions.loc[df_actions["type"] == "Dribble", "Dribble"] = 1
 
-    df_actions_grid = df_actions[["player_id", "Shot", "Pass", "Dribble", "grid_index"]]
-    df_actions_grid = df_actions_grid.groupby(["player_id", "grid_index"], as_index=False).sum()
+    df_actions_grid = df_actions[["player_id", "Shot", "Pass", "Cross", "Dribble", "grid_index", "end_grid_index"]]
+    df_actions_grid = df_actions_grid.groupby(["player_id", "grid_index", "end_grid_index"], as_index=False).sum()
 
     return df_actions_grid
 
@@ -150,12 +168,13 @@ def create_actions_grid(df_actions: pd.DataFrame, football_pitch_size, heatmap_t
 if __name__ == "__main__":
     logging.basicConfig(filename=consts.APP_LOG_PATH, level=logging.DEBUG)
     logger.debug(f"Stats bomb open data local path: {consts.STATSBOMB_OPEN_DATA_LOCAL_PATH}")
-    matches_ids = get_matches_ids(os.path.join(consts.STATSBOMB_OPEN_DATA_LOCAL_PATH, "data", "events"))
     start_time = time.time()
+    matches_ids = get_matches_ids(os.path.join(consts.STATSBOMB_OPEN_DATA_LOCAL_PATH, "data", "events"))
+    #matches_ids = [3754058,3749052]
     df_events = load_events(matches_ids, parallel_processes_count=4)
     #df_events = load_events_single_proc(matches_ids)
-    end_time = time.time()
-    print(f"Load events elapsed time: {end_time-start_time}s")
     df_actions = get_actions(df_events)
     df_actions_grid = create_actions_grid(df_actions, consts.FOOTBALL_PITCH_SIZE, consts.HEATMAP_TILE_SIZE)
     df_actions_grid.to_csv(consts.ACTIONS_GRID_FILE_PATH, index=False)
+    end_time = time.time()
+    print(f"Load events elapsed time: {end_time - start_time}s")
